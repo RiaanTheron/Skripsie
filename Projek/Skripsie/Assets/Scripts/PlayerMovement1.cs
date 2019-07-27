00a57ceba3c7bb30940e6fdff3e5c929e728708e
RiaanTheron <theron.a.riaan@gmail.com>
2019-07-28 01:25:42 +0200

    Commit 1
    Part 1-2
    Player sprie
    Player movement

---
+++ Projek/Skripsie/Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D MyRigidBody;
+
+    private Vector3 change;
+
+    // public Joystick joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+       MyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        change.y = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+    void MoveCharacter()
+    {
+        MyRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+            );
+    }
+
+
+}
